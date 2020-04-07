04f04b129c35d1dea8643c471283d18a2e742447
レイ <rei@l18.work>
2020-04-07 22:31:05 +0900

    ...

--- host/init-php-ini.sh
+++ host/init-php-ini.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 die() {
+	echo $@ 2>&1
 	exit 1
 }
 
 MYPHP=$MYSHARE/php
 
 grepn() {
-	find $inifile || die
+	find $inifile || die no php.ini.
 	ln=`egrep -m 1 -n "include_path[[:space:]]*=" $inifile `
 	l=`echo $ln | sed -e "s/:.*//"`
 	echo $ln | grep $MYPHP && return
@@ -15,7 +16,11 @@ grepn() {
 	sudo vi $inifile +$l
 }
 
-inifile=`php --ini | grep Loaded | sed -e "s|[^/]*||"`
+if [ $# -ge 2 ] ; then
+	inifile=$1
+else
+	inifile=`php --ini | grep Loaded | sed -e "s|[^/]*||"`
+fi
 grepn
 
 if [ x`echo $inifile | grep cli` != x ]; then
